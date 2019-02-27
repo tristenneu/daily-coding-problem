32462906e6325653395803099eeeb5b4b4476190
Tristen Neu <tristenneu@gmail.com>
2019-02-27 13:43:35 -0600

    completed problem 1

---
+++ Problem1/Answer.py
@@ -0,0 +1,45 @@
+'''
+Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+'''
+
+# approach the problem iteratively
+# calculate combinations of equations and compare against k
+# return the first instance of numbers that add up to k
+
+# import pdb; pdb.set_trace()
+
+def calculateK(numberList, k):
+    nums = []
+
+    # iteratively
+    for num in range(len(numberList)):
+        # print(numberList[num])
+        #calculate combinations
+        # newNumberList = list(filter(lambda new: new != num, numberList))
+        addens = [numberList[new] for new in range(len(numberList)) if new != num]
+        # print(addens)
+        for index in range(len(addens)):
+                eq = numberList[num] + addens[index]
+
+                if eq == k:
+                        print('found!')
+                        nums.append(numberList[num])
+                        nums.append(addens[index])
+                        print(nums)
+                        return nums
+                else:
+                        continue
+    print(nums)
+    print(len(nums))
+    if len(nums) == 0:
+            print('False')
+            return False
+    else:
+            print('True')
+            return True
+    
+# calculateK([10, 15, 3, 7], 17)
+calculateK([11, 16, 8, 21], 27)
+calculateK([15, 6, 19, 5], 42)
