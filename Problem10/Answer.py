403c793f1fb50af6fdd5f912f839f84b7bbf04e5
Tristen Neu <tristenneu@gmail.com>
2019-03-08 12:42:13 -0600

    Problem10 solution

--- Problem10/Answer.py
+++ Problem10/Answer.py
@@ -1,3 +1,14 @@
 '''
 Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
 '''
+
+# SOLUTION
+
+import time
+
+def jobscheduler(f, n):
+	time.sleep(n/1000)
+	return f()
+
+print(time.ctime())
+print(jobscheduler(lambda: "Hi! " + time.ctime(), 2000))
